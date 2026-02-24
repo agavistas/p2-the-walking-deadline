26c165b588e460a4339d7d1ccc2760d6c59ef526
agavistas <118552874+agavistas@users.noreply.github.com>
2026-02-24 18:56:24 -0500

    Added the game file this time

---
+++ main.cpp
@@ -0,0 +1,168 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <getopt.h>
+#include "P2random.h"
+
+typedef struct Zombie {
+	std::string name;
+	uint32_t distance;
+	uint32_t speed;
+	uint32_t health;
+	uint32_t lifetime;
+} Zombie;
+
+class Battle {
+	public:
+	Battle(int argc, char** argv) {
+		int option_index = 0, option = 0;
+		opterr = false;
+		struct option longOpts[] = {
+			{"verbose", no_argument, nullptr, 'v'},
+			{"statistics", required_argument, nullptr, 's'},
+			{"median", no_argument, nullptr, 'm'},
+			{"help", no_argument, nullptr, 'h'},
+			{nullptr, 0, nullptr, '\0'}};
+		while ((option = getopt_long(argc, argv, "vs:mh\0", longOpts, &option_index)) != -1) {
+			switch (option) {
+				case 'v': verbose = true; break;
+				case 's': stats = static_cast<uint32_t>(std::stoul(optarg)); break;
+				case 'm': median = true; break;
+				case 'h': printHelp(); exit(0);
+			}
+		} 	
+	}
+
+	void initialize() {
+		std::string buffer;
+		getline(std::cin, buffer, '\n');
+		uint32_t random_seed, max_rand_distance, max_rand_speed, max_rand_health;
+		std::cin >> buffer >> quiver_capacity;
+		std::cin >> buffer >> random_seed;
+		std::cin >> buffer >> max_rand_distance;
+		std::cin >> buffer >> max_rand_speed;
+		std::cin >> buffer >> max_rand_health;
+		P2random::initialize(random_seed, max_rand_distance, max_rand_speed, max_rand_health);
+	}
+
+	void run() {
+		uint32_t round = 1;
+		{ std::string buffer;
+		std::cin >> buffer >> buffer >> nextWave; }
+		while (true) {
+			if (verbose) std::cout << "Round: " << round << "\n";
+			quiver = quiver_capacity;
+			Zombie* endingZombie = moveZombies();
+			if (endingZombie != nullptr) {
+				std::cout << "DEFEAT IN ROUND " << round << "! " << endingZombie->name << " ate your brains!\n";
+				if (stats > 0) std::cout << "Zombies still active: " << eta_queue.size() << "\n";
+				return;
+			}
+			if (round == nextWave) addNewZombies();
+			endingZombie = attackZombies();
+			if (!moreRounds && endingZombie != nullptr) {
+				std::cout <<"VICTORY IN ROUND " << round << "! " << endingZombie->name << " was the last zombie.\n";
+				if (stats > 0) std::cout << "Zombies still active: " << eta_queue.size() << "\n";
+				return;
+			}
+			++round;
+		}
+	}
+
+	private:
+	Zombie* moveZombies() {
+		Zombie* ptr = nullptr;
+		for (Zombie* zomb : zombies) {
+			zomb->distance -= std::min(zomb->distance, zomb->speed);
+			if (zomb->distance == 0 && !ptr) ptr = zomb;
+			if (verbose) std::cout << 
+				"Moved: " << zomb->name << " (distance: " << zomb->distance
+				<< ", speed: " << zomb->speed << ", health: " << zomb->health << ")\n"; 
+		}
+		return ptr;
+	}
+
+	Zombie* attackZombies() {
+		for (; quiver > 0; --quiver) {
+			Zombie* top = eta_queue.top();
+			--(top->health);
+			if (top->health == 0) {
+				eta_queue.pop();
+				if (verbose) std::cout << 
+				"Destroyed: " << top->name << " (distance: " << top->distance
+				<< ", speed: " << top->speed << ", health: " << top->health << ")\n"; 
+			}
+			if (eta_queue.empty()) return top;
+		}
+		return nullptr;
+	}
+
+	void addNewZombies() {
+		uint32_t newZombies;
+		std::string buffer;
+		std::cin >> buffer >> newZombies;
+		for (; newZombies > 0; --newZombies) {
+			Zombie* zombieptr = new Zombie();
+			zombieptr->name = P2random::getNextZombieName();
+			zombieptr->distance = P2random::getNextZombieDistance();
+			zombieptr->speed = P2random::getNextZombieSpeed();
+			zombieptr->health = P2random::getNextZombieHealth();
+			zombieptr->lifetime = 0;
+			zombies.push_back(zombieptr);
+			eta_queue.push(zombieptr);
+
+			if (verbose) std::cout << 
+				"Created: " << zombieptr->name << " (distance: " << zombieptr->distance
+				<< ", speed: " << zombieptr->speed << ", health: " << zombieptr->health << ")\n"; 
+		}
+		std::cin >> buffer >> newZombies;
+		for (; newZombies > 0; --newZombies) {
+			Zombie* zombieptr = new Zombie();
+			std::cin >> zombieptr->name >> buffer >> zombieptr->distance >> 
+			buffer >> zombieptr->speed >> buffer >> zombieptr->health;
+			zombieptr->lifetime = 0;
+			zombies.push_back(zombieptr);
+			eta_queue.push(zombieptr);
+
+			if (verbose) std::cout << 
+				"Created: " << zombieptr->name << " (distance: " << zombieptr->distance
+				<< ", speed: " << zombieptr->speed << ", health: " << zombieptr->health << ")\n"; 
+		}
+		if (std::cin >> buffer) std::cin >> buffer >> nextWave;
+		else moreRounds = false;
+	}
+
+	struct etaGreater {
+		bool operator()(const Zombie* left, const Zombie* right) {
+			if ((left->distance / left->speed) != (right->distance / right->speed)) 
+				return (left->distance / left->speed) > (right->distance / right->speed);
+			if (left->health != right->health) return left->health > right->health;
+			return left->name > right->name;
+		}
+	};
+
+	// game related variables
+	std::vector<Zombie*> zombies;
+	std::priority_queue<Zombie*, std::vector<Zombie*>, etaGreater> eta_queue;
+	bool moreRounds = true;
+	uint32_t nextWave;
+	uint32_t quiver;
+	
+	// init related variables
+	uint32_t quiver_capacity;
+
+	// option related variables
+	void printHelp() {};
+	bool verbose = false;
+	bool median = false;
+	uint32_t stats = 0;
+};
+
+int main(int argc, char** argv) {
+	std::ios::sync_with_stdio(false);
+	Battle game(argc, argv);
+	game.initialize();
+	game.run();
+}
